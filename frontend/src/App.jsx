71e2ec0e03aaec6bca1b74be2516537a400354cd
Joseph Baruch <josephbaruch48@gmail.com>
2023-11-22 10:21:47 -1000

    Finally figured out routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState, useEffect  } from 'react'
 import Display from './Display'
+import Home from './Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
     return (
-        <Router>
-            <div>
-            <h1>Welcome to My React App</h1>
-            <Routes> 
-                <Route path="/display" component={Display} />
-                {/* Add other routes here */}
-            </Routes>
-            </div>
-        </Router>
+        <Routes> 
+            <Route path="/" element={<Home />} />
+            <Route path="/display" element={<Display />} />
+            {/* Add other routes here */}
+        </Routes>
     )
 }
 
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,30 +1,15 @@
 import { useState, useEffect } from 'react'
 
-function Display() {
+function Home() {
 
     const [ invest, setInvest ] = useState({
         value: "What do you want to invest in?"
     })
     const [ button, setButton ] = useState(false)
-    // can now fetch to the backend at the above url
-    // const response = await fetch(`${import.meta.eve.VITE_API_URL}posts`);
-
-    //function valueHandler
-
-    // what do you want to invest in
-        // tech 
-        // hedge funds
-    // dividends or not 
-    // add more factors
-    // gives suggestiongs based on factors
-
-
-
-
 
     return (
         <>
-            <h1>Lets see</h1>
+            <h1>Home</h1>
             <div style={{height: "30px"}}>
                 <input style={{height: "100%", width: "300px"}} value ={invest.value} onChange={setInvest} ></input> 
                 <button style={{height: "100%"}} onClick={setButton}>Go</button>
@@ -36,4 +21,4 @@ function Display() {
 
 
 
-export default Display
+export default Home
