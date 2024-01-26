1a1ae8c12222cba23f5217bb72352d64f091ed95
Joseph Baruch <josephbaruch48@gmail.com>
2024-01-25 19:27:42 -0800

    Initializing the backend and doing a basic set up using a tutorial

--- backend/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-
-def my_get_handler(request, param1, param2):
-    # Your logic here
-    data = {'message': f'GET request received with parameters: {param1}, {param2}'}
-    return JsonResponse(data)
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -34,7 +34,16 @@ function Comp({classes}){
     })
 
     const handleClick = () => {
-       navigate('/display');
+        fetch('http://127.0.0.1:8000')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.users)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        }); 
+        //navigate('/display');
+
     };
 
     return (
